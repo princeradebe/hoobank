4c5fa2948cbff274261f081df1c6eba060ffdc9a
Prince Radebe <princeradebe@gmail.com>
2023-03-14 23:53:20 +0200

    Remove default Tailwind styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
 
   return (
-    <div className="text-3xl font-bold underline max-h-full bg-dimBlue">
+    <div className="max-h-full bg-dimBlue ">
       <Navbar />
     </div>
   )
