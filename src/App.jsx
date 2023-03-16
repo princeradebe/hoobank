b446d12f41d7813035c86ea4ac61198cafa711b7
Prince Radebe <princeradebe@gmail.com>
2023-03-16 22:10:35 +0200

    Add documentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import reactLogo from './assets/react.svg'
-import Navbar from './components/Navbar'
-import './App.css'
+// This code imports the React library, the react logo, the Navbar component, and the App.css file. It also exports the App component, which wraps the Navbar component in a div with a class of max-h-full and bg-dimBlue.
+
+import React from 'react' // Import React library
+import reactLogo from './assets/react.svg' // Import react logo
+import Navbar from './components/Navbar' // Import Navbar component
+import './App.css' // Import App.css file
 
 function App() {
   return (
-    <div className="max-h-full bg-dimBlue">
+    <div className="max-h-full bg-dimBlue"> // Wraps Navbar component
       <Navbar />
     </div>
   )
